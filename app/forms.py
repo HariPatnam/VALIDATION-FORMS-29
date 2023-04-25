4527655b181da6f09e2cc2264d26f7a29acb9efe
HariPatnam <patnamhari5432@gmail.com>
2023-04-25 18:55:16 +0530

    first commit

--- app/forms.py
+++ app/forms.py
@@ -9,10 +9,16 @@ class student(forms.Form):
     AGE=forms.IntegerField()
     EMAIL=forms.EmailField()
     RE_ENTER_EMAIL=forms.EmailField()
+    BOTCATCHER=forms.CharField(max_length=100,widget=forms.HiddenInput,required=False)
     
     def clean(self):
         e=self.cleaned_data['EMAIL']
         r=self.cleaned_data['RE_ENTER_EMAIL']
 
         if e!=r:
-            raise forms.ValidationError('RE ENTER EMAIL NOT MATCHED IN SPECIFIC MAIL ')
+            raise forms.ValidationError('RE ENTER EMAIL NOT MATCHED IN SPECIFIC MAIL')
+        
+    def clean_BOTCATCHER(self):
+        bot=self.cleaned_data['BOTCATCHER']
+        if len(bot)>0:
+            raise forms.ValidationError('inserted by bot or hacker')
