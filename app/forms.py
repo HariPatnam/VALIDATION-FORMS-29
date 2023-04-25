d8f0f99b57a9cf27d431d2518a7be267360ca1c6
HariPatnam <patnamhari5432@gmail.com>
2023-04-25 17:46:53 +0530

    first commit

--- app/forms.py
+++ app/forms.py
@@ -7,3 +7,12 @@ def check_for_m(value):
 class student(forms.Form):
     NAME=forms.CharField(max_length=100,validators=[check_for_m])
     AGE=forms.IntegerField()
+    EMAIL=forms.EmailField()
+    RE_ENTER_EMAIL=forms.EmailField()
+
+    def clean(self):
+        e=self.cleaned_data['EMAIL']
+        r=self.cleaned_data['RE_ENTER_EMAIL']
+
+        if e!=r:
+            raise forms.ValidationError('RE ENTER EMAIL NOT MATCHED IN SPECIFIC MAIL ')
